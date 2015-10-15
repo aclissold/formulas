ca810980089319d711bede2b050792fc20e9b97a
Aaron Giroux <aaronmgiroux@gmail.com>
2015-10-15 16:18:23 -0400

    removed database info

--- public_html/dbconnect.php
+++ public_html/dbconnect.php
@@ -3,7 +3,7 @@ if(!mysql_connect("localhost","username","password"))
 {
      die('oops connection problem ! --> '.mysql_error());
 }
-if(!mysql_select_db("CSE345DATABASE"))
+if(!mysql_select_db("databasename"))
 {
      die('oops database selection problem ! --> '.mysql_error());
 }
