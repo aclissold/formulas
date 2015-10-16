eeca54db27b14b989e7ae115073ce86b04cfcfe5
Aaron Giroux <aaronmgiroux@gmail.com>
2015-10-16 14:30:27 -0400

    set up categories and counts

--- public_html/home.php
+++ public_html/home.php
@@ -30,18 +30,30 @@ $userRow=mysql_fetch_array($res);
 		</div>		
 	</div>
 	<div id="body">
-		<?php $result = mysql_query("SELECT * FROM FORMULA"); ?>
+		<?php $result = mysql_query("SELECT *, USER.USER_NAME AS USERNAME, 
+									CATEGORY.CAT_NAME AS CATEGORYNAME FROM FORMULA INNER JOIN USER ON FORMULA.USER_ID = USER.USER_ID 
+									INNER JOIN CATEGORY ON FORMULA.CAT_ID = CATEGORY.CAT_ID"); ?>
+		<?php $count_all = mysql_num_rows($result); ?>
+		<?php $count_math = mysql_query("SELECT COUNT(CAT_ID) AS MATHROWS FROM FORMULA WHERE CAT_ID = 1"); ?>
+		<?php $count_physics = mysql_query("SELECT COUNT(CAT_ID) AS MATHROWS FROM FORMULA WHERE CAT_ID = 2"); ?>
+		
 		<center>
 			<div id="formula-table" style="width: 80%; margin-top: 100px">
-				<h2 style="color:#a5a5a5">All</h2>
+				<table class="category-table">
+					<tr>
+						<td <h2 style="color:#a5a5a5; margin-bottom:10px">All (<?php echo $count_all ?>)</h2></td>
+						<td <h2 style="color:#a5a5a5; margin-bottom:10px">Math (<?php echo mysql_result($count_math,0) ?>)</h2></td>
+						<td <h2 style="color:#a5a5a5; margin-bottom:10px">Physics (<?php echo mysql_result($count_physics,0) ?>)</h2></td>
+					</tr>
+				</table>
 				<table >
 					<tr style="background:#a5a5a5">
 						<td style="color:#ffffff"><b>Name</b></td>
 						<td style="color:#ffffff"><b>Description</b></td>
 						<td style="color:#ffffff"><b>Formula</b></td>
 						<td style="color:#ffffff"><b>Date</b></td>
-						<td style="color:#ffffff"><b>User Id</b></td>
-						<td style="color:#ffffff"><b>Category Id</b></td>
+						<td style="color:#ffffff"><b>User</b></td>
+						<td style="color:#ffffff"><b>Category</b></td>
 					</tr>
 					<?php while($row = mysql_fetch_array($result)) : ?>
 					<tr>
@@ -49,8 +61,8 @@ $userRow=mysql_fetch_array($res);
 						<td style="width:40%"><?php echo substr($row['FORM_DESC'],0,70).'...'; ?></td>
 						<td style="width:20%"><?php echo $row['FORM_FORMULA']; ?></td>
 						<td style="width:10%"><?php echo date('m-d-Y', strtotime($row['FORM_DATE']) ); ?></td>
-						<td style="width:10%"><?php echo $row['USER_ID']; ?></td>
-						<td style="width:10%"><?php echo $row['CAT_ID']; ?></td>
+						<td style="width:10%"><?php echo $row['USERNAME']; ?></td>
+						<td style="width:10%"><?php echo $row['CATEGORYNAME']; ?></td>
 					</tr>
 					<?php endwhile; ?>
 				</table>
