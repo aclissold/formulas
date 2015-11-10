b2a845c1b592c94ffd05f247c8efb6194353ad9e
Andrew Clissold <andrewclissold@gmail.com>
2015-11-10 17:55:30 -0500

    Style the log in page

--- public_html/index.php
+++ public_html/index.php
@@ -19,50 +19,75 @@ if(isset($_POST['btn-login']))
  }
  else
  {
-  ?>		
+  ?>
         <script>alert('wrong details');</script>
         <?php
  }
- 
+
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Formulas</title>
-	<link rel="stylesheet" href="style.css" type="text/css" />
 	<script src="jquery-2.1.4.min.js"></script>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="login.css" type="text/css" />
 </head>
 <body>
-	<div id="header">
-		<div id="left">
-			<label>Formulas</label>
-		</div>
-		<div id="right">
-			<div id="content">
-			</div>
-		</div>		
-	</div>
-	<center>
-		<div id="login-form">
-			<form method="post">
-				<table align="center" width="30%" border="0">
-					<tr>
-						<td><input type="text" name="email" placeholder="Your Email" required /></td>
-					</tr>
-					<tr>
-						<td><input type="password" name="pass" placeholder="Your Password" required /></td>
-					</tr>
-					<tr>
-						<td><button type="submit" name="btn-login">Sign In</button></td>
-					</tr>
-					<tr>
-						<td><a href="register.php">Sign Up Here</a></td>
-					</tr>
-				</table>
-			</form>
-		</div>
-	</center>
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">f(ormulas)</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="register.php">Register</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+      <form class="form-signin" method="post">
+        <h2>Log in</h2>
+        <input type="text" name="email" id="inputEmail" class="form-control" placeholder="email" required autofocus>
+        <input type="password" name="pass" id="inputPassword" class="form-control" placeholder="password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-login">Log In</button>
+        <a href="register.php">Need an account? Register ›</a>
+      </form>
+
+    </div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
