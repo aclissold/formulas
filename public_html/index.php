1814bcfe5eea5e03798e655dcaba21c2ba6b6d3f
Andrew Clissold <andrewclissold@gmail.com>
2015-11-10 18:59:38 -0500

    Make navbar text flush on all viewport sizes

--- public_html/index.php
+++ public_html/index.php
@@ -53,22 +53,20 @@ if(isset($_POST['btn-login']))
 <body>
     <!-- Static navbar -->
     <nav class="navbar navbar-default navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand navbar-brand-small tex" href="#" data-expr="f">f</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="register.php">Register</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand navbar-brand-small tex" href="#" data-expr="f">f</a>
       </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="register.php">Register</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
     </nav>
 
     <div class="container">
